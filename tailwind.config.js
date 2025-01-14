94e578d4cc413848a98e4cdeff437db2f1a38ac7
KirstenRosen <kirstenr98@gmail.com>
2025-01-13 19:22:01 -0800

    "Deployment attempt update 4"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+@type {import('tailwindcss').Config}
 
 const config = {
   content: [
@@ -27,7 +27,7 @@ const config = {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: ['@tailwindcss/forms'],
 };
 
 export default config;
